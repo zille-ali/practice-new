c2737efd2fff65baacca52a4937bdbd25916bef8
zille-ali <41956489+zille-ali@users.noreply.github.com>
2018-08-23 11:40:59 +0530

    Update Add.js

--- Add.js
+++ Add.js
@@ -1,9 +1,21 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('Add.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-}).listen(8080);
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const staticHref = '/static';
+const publicFolder = path.join(__dirname, '/');
+
+app.use(staticHref, express.static(publicFolder));
+app.use(staticHref, function(req, res) {
+    res.send(404);
+});
+
+app.all('/*', function(req, res) {
+    res.sendFile('Add.html', {root: publicFolder});
+});
+
+app.listen(3000, function(){
+    console.log('running');
+                });
+
+
