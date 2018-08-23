5bd42162559995d22da91df010168fc3b2b4ad84
zille-ali <41956489+zille-ali@users.noreply.github.com>
2018-08-23 18:18:16 +0530

    Update Add.js

--- Add.js
+++ Add.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var fs = require('fs');
 var app =express();
-app.get('/',function(req, res) {
+var port = process.env.PORT || 3000;
+
+
+app.get('/', function(req, res) {
   fs.readFile('Add.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
 })});
-app.listen(3000,function(){});
+app.listen(port);
 
 
